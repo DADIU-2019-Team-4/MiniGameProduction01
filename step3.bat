9b446d3886632e6997b155631440621b1601a76d
MichaeliusDenModige <s143049@student.dtu.dk>
2019-09-02 16:45:24 +0200

    Delete step3.bat

--- step3.bat
+++
@@ -1,3 +0,0 @@
-"C:\Program Files\Unity\Hub\Editor\2019.2.3f1\Editor\Unity.exe" -projectPath "C:\Program Files\Unity Hub\Projects\DADIU Minigame 1" -executeMethod
-UnityEditor.Pipeline.BuildAndroid
-Pause
