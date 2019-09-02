02542462d9af10efd5dacbd2c6658344a76c1815
MichaeliusDenModige <s143049@student.dtu.dk>
2019-09-02 16:31:50 +0200

    Add files via upload

---
+++ step3.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Unity\Hub\Editor\2019.2.3f1\Editor\Unity.exe" -projectPath "C:\Program Files\Unity Hub\Projects\DADIU Minigame 1" -executeMethod
+UnityEditor.Pipeline.BuildAndroid
+Pause
