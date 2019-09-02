c8e0003ef0133272cb8f548c6a5cd0dcfc97b822
MichaeliusDenModige <s143049@student.dtu.dk>
2019-09-02 14:46:19 +0200

    Added TestScene
    Works on Windows!

---
+++ Assets/Scripts/BallHop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallHop : MonoBehaviour
+{
+    public int VelocityUpPower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = new Vector3(0, VelocityUpPower, 0);
+    }
+}
