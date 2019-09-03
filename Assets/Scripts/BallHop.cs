ab3809c529f0a500b28acbea73e33f1679888dff
MichaeliusDenModige <s143049@student.dtu.dk>
2019-09-03 09:51:02 +0200

    Physics Test

--- Assets/Scripts/BallHop.cs
+++ Assets/Scripts/BallHop.cs
@@ -5,22 +5,31 @@ using UnityEngine;
 public class BallHop : MonoBehaviour
 {
     public int VelocityUpPower;
+    public int MaxRandomX;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var rigidbody = GetComponent<Rigidbody>();
+        var pos = rigidbody.position;
+        var vel = rigidbody.velocity;
+
+        if (-10 > pos.x || pos.x > 10)
+            rigidbody.velocity = new Vector3(-vel.x, vel.y, vel.z);
+
     }
 
     void OnMouseDown()
     {
+        float randomX = Random.Range(-MaxRandomX, MaxRandomX);
+
         var rigidbody = GetComponent<Rigidbody>();
-        rigidbody.velocity = new Vector3(0, VelocityUpPower, 0);
+        rigidbody.velocity = new Vector3(randomX, VelocityUpPower, 0);
     }
 }
