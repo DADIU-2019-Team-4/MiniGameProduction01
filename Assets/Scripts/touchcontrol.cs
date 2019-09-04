57e6def0f024d2dbed33fafde34ab144f3a456da
Roxanne van Dam <roxanne.vandam@outlook.com>
2019-09-04 15:27:23 +0200

    added touch controls to prototype3 scene

--- Assets/Scripts/touchcontrol.cs
+++ Assets/Scripts/touchcontrol.cs
@@ -1,21 +1,24 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class touchcontrol : MonoBehaviour
+public class TouchControl : MonoBehaviour
 {
     private List<addForce> balls;
     private Vector3 position;
     private float width;
-    private float height;
-    public Camera camera;
-    private Vector3 currentMouseClickWorldSpace;
+
+    // mobile input variables
+    private Vector3 _firstTouchPos;
+    private Vector3 _lastTouchPos;
+    // minimum distance for a swipe to be registered
+    private float _minSwipeDistance;
 
     // Start is called before the first frame update
     void Start()
     {
         width = (float)Screen.width;
-        height = (float)Screen.height;
+        _minSwipeDistance = Screen.height * 0.15f; // 15% height of the screen
     }
 
     // Update is called once per frame
@@ -24,31 +27,80 @@ public class touchcontrol : MonoBehaviour
         balls = new List<addForce>(FindObjectsOfType<addForce>());
         //Debug.Log(balls.Capacity);
 
+#if UNITY_EDITOR || UNITY_STANDALONE
+        PcInput();
+#elif UNITY_ANDROID || UNITY_IOS
+        MobileInput();
+#endif
+    }
 
-        if (Input.touchCount > 0 || Input.GetMouseButtonDown(0))
+    private void PcInput()
+    {
+        if (Input.GetMouseButtonDown(0))
         {
-
-            currentMouseClickWorldSpace = camera.ScreenToWorldPoint(Input.mousePosition);
-
-            Debug.Log("clicked x is "+ Input.mousePosition);
-            
-                if(Input.mousePosition.x < width/2)
-                {
-                    foreach(addForce ball in balls)
-                    {
-                        ball.throwLeft();
-                    }
-                    Debug.Log("clicked left side");
-                }
-                else
-                {
-                    foreach (addForce ball in balls)
-                    {
-                        ball.throwRight();
-                    }
-                    Debug.Log("clicked right side");
-                }
-            
+            if (Input.mousePosition.x < width / 2)
+                ThrowLeft();
+            else
+                ThrowRight();
         }
+    }
+
+    private void MobileInput()
+    {
+        // if user is touching the screen with a single touch...
+        if (Input.touchCount == 1)
+        {
+            // get the touch
+            Touch touch = Input.GetTouch(0);
+            // check for the first touch
+            if (touch.phase == TouchPhase.Began)
+            {
+                _firstTouchPos = touch.position;
+                _lastTouchPos = touch.position;
+            }
+            // update the last position based on where it moved
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                _lastTouchPos = touch.position;
+            }
+            // check if the finger is removed from the screen
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                // last touch position
+                _lastTouchPos = touch.position;
+
+                // difference vector
+                var differenceVec = _lastTouchPos - _firstTouchPos;
+
+                // check if swipe distance is greater than 15% of the screen height
+                if (!(Math.Abs(differenceVec.x) > _minSwipeDistance) &&
+                    !(Math.Abs(differenceVec.y) > _minSwipeDistance)) return;
+
+                // check if the swipe is vertical
+                if (!(Mathf.Abs(differenceVec.x) <= Mathf.Abs(differenceVec.y))) return;
+
+                // check if we have swiped up
+                if (!(_lastTouchPos.y > _firstTouchPos.y)) return;
+
+                // swiped on left side of the screen
+                if (_lastTouchPos.x < width / 2)
+                    ThrowLeft();
+                // swiped on right side of the screen
+                else
+                    ThrowRight();
+            }
+        }
+    }
+
+    private void ThrowLeft()
+    {
+        foreach (addForce ball in balls)
+            ball.throwLeft();
+    }
+
+    private void ThrowRight()
+    {
+        foreach (addForce ball in balls)
+            ball.throwRight();
     }
 }
