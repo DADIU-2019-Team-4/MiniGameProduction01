721046c0a55034d385c121799ad9010dc07c7291
Roxanne van Dam <roxanne.vandam@outlook.com>
2019-09-06 13:42:20 +0200

    made basic phase 6 scene and added DoTween plugin for tweening

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    [SerializeField]
+    private float _timeUntilPaper = 2f;
+    [SerializeField]
+    private float _timeUntilEnd = 2f;
+
+    [SerializeField]
+    private GameObject _timmy;
+
+    [SerializeField]
+    private GameObject _paperBall;
+
+    [SerializeField]
+    private Fade _fade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EndSequence());
+    }
+
+    private IEnumerator EndSequence()
+    {
+        yield return new WaitForSeconds(_timeUntilPaper);
+        _timmy.SetActive(false);
+        _paperBall.SetActive(true);
+        yield return new WaitForSeconds(_timeUntilEnd);
+        _fade.gameObject.SetActive(true);
+    }
+}
